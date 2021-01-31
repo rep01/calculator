2641b5a123f790b3f67bcfcbf854d296febedaa1
Randalf <74125730+rep01@users.noreply.github.com>
2021-02-01 08:32:53 +1100

    Add files via upload

--- C++ Calculator/C++ Calculator/C++ Calculator.cpp
+++ C++ Calculator/C++ Calculator/C++ Calculator.cpp
@@ -25,12 +25,18 @@ constexpr double percentageOf(const double x, const double y) {
 }
 
 int main() {
+    SetConsoleTitleA("Calculator");
     char exit;
-
     do {
         double x, y; int op;
 
         std::cout << "1) Add:\n2) Divide:\n3) Multiply:\n4) Subtraction:\n5) Percentage Of:\n6) Mean(Not Done)\n7) Average(Not Done)\n8) Mode(Not Done)\n";
+
+        // and that, done poorly would look like
+        // using namespace std;
+        // cout << "1) Add:" << endl;
+        // cout << "2) Divide:" << endl;
+        // you get the point
 
         std::cin >> op;
 
@@ -64,7 +70,7 @@ int main() {
             std::cout << "Input Invalid \n";
             break;
         }
-        Color(7);
+        Color(7); // changes color back to default, i only want the answer to be coloured.
         std::cout << "\n Would you like to perform any other operations? y/n\n";
 
         std::cin >> exit;
